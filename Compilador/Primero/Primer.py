55c4a5fbfc8750d3de7eabd635a8acf273f679da
HectorManu <le19081442@itmerida.edu.mx>
2023-02-09 10:24:09 -0600

    cree la carpeta compilador y añadi el primer ejercicio en el cual tengo que usar interfaz gráfica con python

---
+++ Compilador/Primero/Primer.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+raiz = Tk()
+
+raiz.title("Primer Ejercicio")
+
+#raiz.resizable() este admite valores booleanos el primero es a lo ancho y el segundo a lo alto de la ventana
+
+raiz.mainloop() # SIEMPRE AL FINAL es para que la ventana este en un buble infinito
---
+++ Notas.md
@@ -0,0 +1,9 @@
+# Notas
+## 07/02/2023
+
+Operaón de las asignaciones la utilizaremos para evaluar el sintaxis y las reglas 
+
+Sintaxis | Ejemplo
+--- | ---
+----|----
+
