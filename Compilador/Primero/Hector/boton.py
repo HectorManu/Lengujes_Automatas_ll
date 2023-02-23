7493cb080ae6998a6ce800b6f9c7f063d812137c
HectorManu <le19081442@itmerida.edu.mx>
2023-02-23 12:36:45 -0600

    agregue cosas

--- Compilador/Primero/Hector/boton.py
+++ Compilador/Primero/Hector/boton.py
@@ -1,39 +1,43 @@
-import tkinter as tk
+# import tkinter as tk
 
-class Ventana:
-    def __init__(self):
-        self.ventana = tk.Tk()
-        self.ventana.geometry('300x200')
+# class Ventana:
+#     def __init__(self):
+#         self.ventana = tk.Tk()
         
-        self.boton = tk.Button(self.ventana, text='Imprimir tabla de lexemas y tipos de datos', command=self.mostrar_mensaje)
-        self.boton.pack(pady=20)
+#         self.boton = tk.Button(self.ventana, text='Imprimir tabla de lexemas y tipos de datos', command=self.mostrar_mensaje)
+#         self.boton.pack(pady=20)
         
-        self.label_mensaje = tk.Label(self.ventana, text='', font=('Arial', 14))
-        self.label_mensaje.pack()
+#         self.label_mensaje = tk.Label(self.ventana, text='', font=('Arial', 14))
+#         self.label_mensaje.pack()
         
-    def mostrar_mensaje(self):
-        self.boton.destroy()
-        self.boton = tk.Button(self.ventana, text="Finalizar",command=self.fin)
-        self.boton.pack(pady=20)
+#     def mostrar_mensaje(self):
+#         self.boton.destroy()
+#         self.boton = tk.Button(self.ventana, text="Finalizar",command=self.fin)
+#         self.boton.pack(pady=20)
 
-        self.archivo = open('./datos.txt')
-        self.mensaje = self.archivo.readline()
-        print(type(self.mensaje))
-        self.archivo.close()
+#         self.archivo = open('./datos.txt')
+#         self.mensaje = self.archivo.readlines()
+#         print(len(self.mensaje))
+#         self.archivo.close()
 
-        self.label_mensaje.config(text=self.mensaje)
+#         self.label_mensaje.config(text=self.mensaje)
     
-    def fin(self):
-        self.ventana.destroy()
+#     def fin(self):
+#         self.ventana.destroy()
         
-    def ejecutar(self):
-        self.ventana.mainloop()
+#     def ejecutar(self):
+#         self.ventana.mainloop()
 
 
 
-mi_ventana = Ventana()
-mi_ventana.ejecutar()
+# mi_ventana = Ventana()
+# mi_ventana.ejecutar()
 
-# with open('./datos.txt') as archivo:
-#     contenido = archivo.read()
-#     print(contenido)
+with open('./datos.txt') as archivo:
+    lineas = archivo.read()
+    lineas = [linea.strip() for linea in lineas]
+
+print(lineas)
+
+
+
