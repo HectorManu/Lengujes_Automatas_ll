6c365395bb98a9d6b0eb712480d71f6fd342ad74
HectorManu <le19081442@itmerida.edu.mx>
2023-02-26 00:05:03 -0600

    agregue en boton el lexema pero aún falta añadir el tipo de dato

--- Compilador/Primero/Hector/boton.py
+++ Compilador/Primero/Hector/boton.py
@@ -33,11 +33,54 @@
 # mi_ventana = Ventana()
 # mi_ventana.ejecutar()
 
-with open('./datos.txt') as archivo:
-    lineas = archivo.read()
-    lineas = [linea.strip() for linea in lineas]
+import tkinter as tk
+from tkinter import ttk
 
-print(lineas)
+
+# Caracteres a eliminar 
+coma = ','
+puntocoma = ';'
+parentesis1 = '('
+parentesis2 = ')'
+corchete1 = '{'
+corchete2 = '}'
+saltolinea = '\n'
+comilla = '"'
+
+
+# Abrir el archivo y leer las líneas
+with open('./datos.txt', 'r') as f:
+    leyendo = f.read()
+    separando_lineas = leyendo.split(' ')
+    print(leyendo)
+    arreglosinespacios = separando_lineas
+    for i in range(len(arreglosinespacios)):
+        arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(puntocoma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(saltolinea,'').replace(comilla,'')
+
+    sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
+    sinrepeticion = set(sinvacio)
+    #print(sinrepeticion)
+
+#es agarrar un if y ver si una linea empieza en ntr y en van y en echi entonces cuando encuentre un ; todas las variables anteriores son ntr y entonces en el lugar donde se encuentre la primera varialbe estara ntr xd y pues así con todas las demás hasta encontrar un ntr nuevo o algo así pero debe recorrerse todo el arreglo de readlines  mm interesante 
+
+
+
+# Crear la ventana
+ventana = tk.Tk()
+ventana.title('Tabla de líneas')
+
+# Crear la tabla
+tabla = ttk.Treeview(ventana, columns=('tipo',))
+tabla.heading('tipo', text='Tipos de datos')
+tabla.heading('#0', text='Lexemas')
+
+# Agregar lasinrepeticion as a la tabla
+for i, tipo in enumerate(sinrepeticion):
+    tabla.insert('', 'end', text=(tipo.strip(),),values=str(i+1))
+
+# Empaquetar la tabla y mostrar la ventana
+tabla.pack(expand=True, fill='both')
+ventana.mainloop()
 
 
 
