50b2b31056a8521ef6864e9edf14ed170134a65f
HectorManu <le19081442@itmerida.edu.mx>
2023-02-27 14:49:05 -0600

    agregue la tabla y otras cosas

--- Compilador/Primero/Hector/idtype.py
+++ Compilador/Primero/Hector/idtype.py
@@ -7,18 +7,30 @@ corchete1 = '{'
 corchete2 = '}'
 saltolinea = '\n'
 comilla = '"'
+suma = '+'
+resta = '-'
+multi = '*'
+igual = '='
+subarreglo = []
 
 # Abrir archivo para imprimir en tabla
 with open('./datos.txt', 'r') as f:
     leyendo = f.read()
     separando_lineas = leyendo.split(' ')
+    subarreglo = leyendo.split('\n')
     # print(leyendo)
     arreglosinespacios = separando_lineas
     for i in range(len(arreglosinespacios)):
-        arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(comilla,'')
+        arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(comilla,'').replace(suma,'').replace(resta,'').replace(suma,'').replace(multi,'').replace(igual,'')
 
 sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
-sinrepeticion = set(sinvacio)
+sinrepeticion = set(arreglosinespacios)
+
+print(sinvacio)
 
 
-print(separando_lineas)
+for i in range(len(sinvacio)):
+    if '\n' in sinvacio[i]:
+        print("encontre el caracter")
+        subarreglo = sinvacio[i].split(';')
+        sinvacio[i:i+1] = subarreglo
--- Compilador/Primero/Hector/main.py
+++ Compilador/Primero/Hector/main.py
@@ -9,6 +9,7 @@ class Ventana:
         self.identificados = []
         self.sinrepeticion = []
         
+        # HACEMOS EL BOTON
         self.boton = tk.Button(self.master, text='Imprimir tabla de lexemas y tipos de datos', command=self.mostrar_lexematipo)
         self.boton.pack(pady=20)
 
@@ -19,7 +20,7 @@ class Ventana:
         # self.boton.pack(pady=20)
         self.master.title('Tabla lexemas')
 
-        # # Caracteres a eliminar 
+        # # Caracteres a eliminar QUE NO APARECERAN EN LA TABLA
         coma = ','
         puntocoma = ';'
         parentesis1 = '('
@@ -81,63 +82,6 @@ raiz = tk.Tk()
 mi_ventana = Ventana(raiz)
 raiz.mainloop()
 
-# import tkinter as tk
-# from tkinter import ttk
-
-
-# # Caracteres a eliminar 
-# coma = ','
-# puntocoma = ';'
-# parentesis1 = '('
-# parentesis2 = ')'
-# corchete1 = '{'
-# corchete2 = '}'
-# saltolinea = '\n'
-# comilla = '"'
-
-
-# # Abrir el archivo y leer las líneas
-# with open('./datos.txt', 'r') as f:
-#     leyendo = f.read()
-#     separando_lineas = leyendo.split(' ')
-#     print(leyendo)
-#     arreglosinespacios = separando_lineas
-#     for i in range(len(arreglosinespacios)):
-#         arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(puntocoma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(saltolinea,'').replace(comilla,'')
-
-#     sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
-#     sinrepeticion = set(sinvacio)
-#     #print(sinrepeticion)
-
-# #Lo que haremos para identificar los tipos de datos será lo siguiente: 
-# # Primero recorreremos cada línea buscando una declaración de variable es decir si empieza con ntr van echi entonces lo que sigue es un nombre de variable entonces realizaremos un arreglo por cada variable que no se repita es decir guardaremos los valores y cuando lo comparemos en la otra lista 
-
-# # Arreglos para tipos de datos
-# ntr = [] # Enteros
-# van = [] # Reales
-# echi = [] # Cadena
-
-# with open('./datos.txt','r') as f:
-#     arreglolineas = f.readlines()
-#     print(arreglolineas)
-
-
-# # Crear la ventana
-# ventana = tk.Tk()
-# ventana.title('Tabla de líneas')
-
-# # Crear la tabla
-# tabla = ttk.Treeview(ventana, columns=('tipo',))
-# tabla.heading('tipo', text='Tipos de datos')
-# tabla.heading('#0', text='Lexemas')
-
-# # Agregar lasinrepeticion as a la tabla
-# for i, tipo in enumerate(sinrepeticion):
-#     tabla.insert('', 'end', text=(tipo.strip(),),values=(tipo.strip(),))
-
-# # Empaquetar la tabla y mostrar la ventana
-# tabla.pack(expand=True, fill='both')
-# ventana.mainloop()
 
 
 
