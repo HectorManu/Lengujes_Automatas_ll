2303589ab9001e04efb4a4da5b2b9f18d0cbb7af
HectorManu <le19081442@itmerida.edu.mx>
2023-02-28 01:17:03 -0600

    agregue el identificar las variables en arreglos entonces ahora solo queda hacer que se imprima la tabla y después hacer la cosa d elas expresiones regulares y listo

--- Compilador/Primero/Hector/idtype.py
+++ Compilador/Primero/Hector/idtype.py
@@ -1,36 +1,88 @@
-# # Caracteres a eliminar 
-coma = ','
-puntocoma = ';'
-parentesis1 = '('
-parentesis2 = ')'
-corchete1 = '{'
-corchete2 = '}'
-saltolinea = '\n'
-comilla = '"'
-suma = '+'
-resta = '-'
-multi = '*'
-igual = '='
-subarreglo = []
+# # # Caracteres a eliminar 
+# coma = ','
+# puntocoma = ';'
+# parentesis1 = '('
+# parentesis2 = ')'
+# corchete1 = '{'
+# corchete2 = '}'
+# saltolinea = '\n'
+# comilla = '"'
+# suma = '+'
+# resta = '-'
+# multi = '*'
+# igual = '='
+# subarreglo = []
 
-# Abrir archivo para imprimir en tabla
-with open('./datos.txt', 'r') as f:
-    leyendo = f.read()
-    separando_lineas = leyendo.split(' ')
-    subarreglo = leyendo.split('\n')
-    # print(leyendo)
-    arreglosinespacios = separando_lineas
-    for i in range(len(arreglosinespacios)):
-        arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(comilla,'').replace(suma,'').replace(resta,'').replace(suma,'').replace(multi,'').replace(igual,'')
-
-sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
-sinrepeticion = set(arreglosinespacios)
-
-print(sinvacio)
+# enteros_ntr = []
+# reales_van = []
+# cadena_echi = []
 
 
-for i in range(len(sinvacio)):
-    if '\n' in sinvacio[i]:
-        print("encontre el caracter")
-        subarreglo = sinvacio[i].split(';')
-        sinvacio[i:i+1] = subarreglo
+# # Abrir archivo para imprimir en tabla
+# with open('./datos.txt', 'r') as f:
+#     leyendo = f.read()
+#     separando_lineas = leyendo.split(' ')
+#     subarreglo = leyendo.split('\n')
+#     # print(leyendo)
+#     arreglosinespacios = separando_lineas
+#     for i in range(len(arreglosinespacios)):
+#         arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(comilla,'').replace(suma,'').replace(resta,'').replace(suma,'').replace(multi,'').replace(igual,'')
+
+# sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
+# sinrepeticion = set(arreglosinespacios)
+
+# print(sinvacio)
+
+
+
+
+# cadena = [";\nHola; amigo, como estas?",'hola;']
+# nueva_cadena = ""
+
+# for i in range(len(cadena)):
+#     tempo = cadena[i]
+#     print(type(tempo))
+#     for caracter in tempo:
+#         if caracter == "\n":
+#             nueva_cadena += " \n "
+#         else:
+#             nueva_cadena += caracter
+#     cadena[i] = nueva_cadena
+#     nueva_cadena = ''
+
+# print(cadena)
+
+
+cadena = ["NTR",'RACH','rasd',';','\n','NTR','R2C','asdfa','=',';','VAN','asd','ijlkjlkjñlkj',';','ECHI','hola',';']
+enteros = []
+reales = []
+cadenas = []
+
+for i in range(len(cadena)):
+    tempo = cadena[i]
+    if 'NTR' == tempo:
+        for f in range(len(cadena)):
+            f += i
+            if cadena[f+1] == ';' or cadena[f+1] == '\n' or cadena[f+1] == '=':
+                break
+            else:
+                enteros.append(cadena[f+1])
+    if 'VAN' == tempo:
+        for f in range(len(cadena)):
+            f += i
+            if cadena[f+1] == ';' or cadena[f+1] == '\n' or cadena[f+1] == '=':
+                break
+            else:
+                reales.append(cadena[f+1])
+    if 'ECHI' == tempo:
+        for f in range(len(cadena)):
+            f += i
+            if cadena[f+1] == ';' or cadena[f+1] == '\n' or cadena[f+1] == '=':
+                break
+            else:
+                cadenas.append(cadena[f+1])
+
+
+print(enteros)
+print(reales)
+print(cadenas)
--- Compilador/Primero/Hector/main.py
+++ Compilador/Primero/Hector/main.py
@@ -29,31 +29,48 @@ class Ventana:
         corchete2 = '}'
         saltolinea = '\n'
         comilla = '"'
+        suma = '+'
+        resta = '-'
+        multi = '*'
+        igual = '='
 
         # Abrir archivo para imprimir en tabla
         with open('./datos.txt', 'r') as f:
             leyendo = f.read()
             separando_lineas = leyendo.split(' ')
-            # print(leyendo)
-            arreglosinespacios = separando_lineas
-            for i in range(len(arreglosinespacios)):
-                arreglosinespacios[i] = arreglosinespacios[i].replace(coma,'').replace(puntocoma,'').replace(parentesis1,'').replace(parentesis2,'').replace(corchete1,'').replace(corchete2,'').replace(saltolinea,'').replace(comilla,'')
+            self.arreglosinespacios = separando_lineas
+            nueva_arreglosinespacios = ""
 
-        sinvacio = [elemento for elemento in arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
-        self.sinrepeticion = set(sinvacio)
+            for i in range(len(self.arreglosinespacios)):
+                tempo = self.arreglosinespacios[i]
+                for caracter in tempo:
+                    if caracter == "\n":
+                        nueva_arreglosinespacios += " \n "
+                    else:
+                        nueva_arreglosinespacios += caracter
+                self.arreglosinespacios[i] = nueva_arreglosinespacios
+                nueva_arreglosinespacios = ''
+            
+            cadenatemporal = " ".join(self.arreglosinespacios)
+            self.arreglosinespacios = cadenatemporal.split(" ")
+
+                
+
+        self.sinvacio1 = [elemento for elemento in self.arreglosinespacios if elemento != ""] #eliminar variables vacias después de eliminar los puntos y comas y corechetes y eso
+        self.sinvacio = [elemento for elemento in self.sinvacio1 if elemento !='\n']
+        self.sinrepeticion = set(self.sinvacio)
         
-
         #creando tabla
         tabla = ttk.Treeview(self.master,columns=('tipo','lexema',))
         tabla.heading('lexema',text='Lexemas')
         tabla.heading('tipo', text='Tipos de datos')
         tabla.heading('#0',text='')
 
-        sinrepeticion2 = self.identificar_tipos()
+        sinrepeticion2 = self.identifica_enteros()
 
         # Agregar las inrepeticion as tabla
         for i, tipo in enumerate(self.sinrepeticion):
-            tabla.insert('', 'end', text='',values=(tipo.strip(),sinrepeticion2[i].strip()))
+            tabla.insert('', 'end', text=i,values=(tipo.strip(),sinrepeticion2[i].strip()))
 
         # Configurando ancho de columna
         tabla.column('#0',width=0)
@@ -61,21 +78,46 @@ class Ventana:
         # Empaquetatabla y mostrar la master
         tabla.pack(expand=True, fill='both')
         self.boton.destroy()#destuir el boton y dejar nomas la tabla
-        return self.sinrepeticion
+        # self.identificar_errores()
     
-    def identificar_tipos(self):
+    def identifica_enteros(self):
         arreglo1 = list(self.sinrepeticion)
+        arreglo2 = self.sinvacio1
+        arreglo2 = [elemento for elemento in arreglo2 if elemento !=',']
+        arreglo2 = [elemento for elemento in arreglo2 if elemento !='(']
+        arreglo2 = [elemento for elemento in arreglo2 if elemento !=')']
+        arreglo2 = [elemento for elemento in arreglo2 if elemento !='{']
+        arreglo2 = [elemento for elemento in arreglo2 if elemento !='}']
+        print(arreglo2)
 
-        with open('datos.txt','r') as f:
-            lineas = f.readlines()
-        
+
+        for i in range(len(arreglo2)):
+            if i == 'NTR':
+                if i+1 == ';' or i+1 == '\n' or i+1 == '=':
+                    print('llego breark')
+                    break
+                print('encontrontr')
+
         
         
         return arreglo1
+
     
-    def identificar_errore(self):
-        arreglo2 = []
-        return arreglo2
+    def identificar_errores(self):
+        segunda_ventana = tk.Toplevel()
+        segunda_ventana.title("Errores")
+        tabla = ttk.Treeview(segunda_ventana,columns=('tipo','lexema',))
+        tabla.heading('lexema',text='Lexemas')
+        tabla.heading('tipo', text='Tipos de datos')
+        tabla.heading('#0',text='')
+
+        tabla.insert(parent='',index=0,iid=0,text='error',values=('uno','dos'))
+
+        
+        tabla.pack(fill=tk.BOTH,expand=1)
+
+
+
         
 
 raiz = tk.Tk()
